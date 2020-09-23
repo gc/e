342cc9b6983f0df3d161be0c5b15476fde086419
gc <gc@users.noreply.github.com>
2020-09-24 05:23:10 +1000

    import sorting, fix lint

--- src/lib/object.ts
+++ src/lib/object.ts
@@ -1,11 +1,11 @@
-export function entries<T extends {}>(obj: T) {
+export function entries<T extends Record<PropertyKey, unknown>>(obj: T) {
 	return Object.entries(obj) as [keyof T, T[keyof T]][];
 }
 
-export function values<T extends {}>(obj: T) {
+export function values<T extends Record<PropertyKey, unknown>>(obj: T) {
 	return Object.values(obj) as T[keyof T][];
 }
 
-export function keys<T extends {}>(obj: T) {
+export function keys<T extends Record<PropertyKey, unknown>>(obj: T) {
 	return Object.keys(obj) as (keyof T)[];
 }
