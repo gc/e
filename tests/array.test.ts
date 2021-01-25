63cc99c34bcb24a1440030104b6c3761a3a1775a
gc <30398469+gc@users.noreply.github.com>
2021-01-25 21:03:40 +1100

    add shuffleArr/sumArr

--- tests/array.test.ts
+++ tests/array.test.ts
@@ -1,4 +1,4 @@
-import { chunk, randArrItem, uniqueArr } from '../src/lib/array';
+import { chunk, randArrItem, shuffleArr, sumArr, uniqueArr } from '../src/lib/array';
 
 describe('chunk', () => {
 	test('GIVEN chunks is a whole number THEN throws TypeError', () => {
@@ -30,4 +30,13 @@ test('randArrItem', () => {
 test('uniqueArr', () => {
 	expect(uniqueArr([1, 1, 1])).toEqual([1]);
 	expect([1, 2, 3, 4, 5]).toEqual(uniqueArr([1, 2, 3, 4, 5]));
+});
+
+test('sumArr', () => {
+	expect(sumArr([5, 5, 5])).toEqual(15);
+	expect(sumArr([])).toEqual(0);
+});
+
+test('shuffleArr', () => {
+	expect(shuffleArr([1, 2, 3])).toEqual(expect.arrayContaining([1, 2, 3]));
 });
